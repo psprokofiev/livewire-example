35c7a60f82086abe517c23a26e8f8609d62ff7d9
psprokofiev <ps.prokofiev@gmail.com>
2023-07-27 09:53:36 +0300

    query, reset page

--- app/Http/Livewire/Jobs/Index.php
+++ app/Http/Livewire/Jobs/Index.php
@@ -4,15 +4,32 @@ namespace App\Http\Livewire\Jobs;
 
 use App\Concerns\Livewire\WithPagination;
 use App\Models\Job;
+use Illuminate\Contracts\Database\Query\Builder;
 use Livewire\Component;
 
 class Index extends Component
 {
     use WithPagination;
 
+    public string $search = '';
+
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'page'   => ['except' => 1],
+    ];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
         $jobs = Job::query()
+            ->when($this->search, function (Builder $query): void {
+                $query->where('title', 'like', "%{$this->search}%")
+                    ->orWhere('content', 'like', "%{$this->search}%");
+            })
             ->latest('id')
             ->paginate();
 
--- resources/views/livewire/jobs/index.blade.php
+++ resources/views/livewire/jobs/index.blade.php
@@ -1,4 +1,21 @@
 <div>
+    <div class="form-group mb-3">
+        <input type="search" class="form-control"
+               wire:model.debounce.2000ms="search"
+               wire:loading.attr="disabled"
+               placeholder="Search..." required />
+    </div>
+
+    <div class="mb-3 text-end">
+        @if($search)
+            <span>Searching:</span>
+            <i>{{ $search }}</i>, found: {{ $jobs->total() }}
+            <a href="#" wire:click="$set('search', '')">Reset</a>
+        @else
+            <span>Total found: {{ $jobs->total() }}
+        @endif
+    </div>
+
     <table class="table table-striped align-middle">
         <tr>
             <th>#</th>
@@ -6,9 +23,13 @@
             <th style="min-width:3rem;">&nbsp;</th>
             <th>Status</th>
         </tr>
-        @foreach($jobs as $job)
+        @forelse($jobs as $job)
             @livewire('jobs.index.job', compact('job'), key($job->id))
-        @endforeach
+        @empty
+            <tr>
+                <td colspan="4" class="text-center">Nothing found</td>
+            </tr>
+        @endforelse
     </table>
 
     {{ $jobs->links() }}
